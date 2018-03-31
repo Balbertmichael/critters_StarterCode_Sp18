bb15999238546fa570eda638169d07d25299c9c9
Balbertmichael <balbertmichael@utexas.edu>
2018-03-31 12:11:31 -0500

    Added MakeCritter to Controller

--- src/assignment4/Controller.java
+++ src/assignment4/Controller.java
@@ -1,10 +1,17 @@
 package assignment4;
 
+import java.io.File;
+import java.lang.reflect.Modifier;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 public class Controller {
@@ -16,33 +23,58 @@ public class Controller {
 	public Button togglePlayButton;
 	@FXML
 	public Slider speedSlider;
-	
+	@FXML
+	public ChoiceBox<String> critterChoiceBox;
+	@FXML
+	public TextField critterNumber;
+	@FXML
+	public Button addCritter;
+
 	private CritterWorldView world;
 	private WorldStepTimer worldStepTimer;
-	
+
 	public void initialize() {
-		
-		world = new CritterWorldView(anchorContainer);		
+		world = new CritterWorldView(anchorContainer);
+		populateChoiceBox();
 		worldStepTimer = new WorldStepTimer(world, speedSlider);
 		viewPane.setContent(world);
-
 	}
-	
+
 	public void updateView() {
 		Critter.worldTimeStep();
 		Critter.displayWorld(world);
 	}
-	
+
 	public void toggleAnimateView() {
-		if(world.isAnimateOn()) {
+		if (world.isAnimateOn()) {
 			world.setAnimate(false);
 			worldStepTimer.stop();
 			togglePlayButton.setText("Play");
-		}
-		else {
+		} else {
 			world.setAnimate(true);
 			worldStepTimer.start();
 			togglePlayButton.setText("Stop");
+		}
+	}
+
+	private void populateChoiceBox() {
+		critterChoiceBox.getItems().addAll(Main.critter_list);
+		if (!Main.critter_list.isEmpty()) {
+			critterChoiceBox.setValue(Main.critter_list.get(0));
+		}
+	}
+
+	public void controllerMakeCritter() {
+		String inputNum = critterNumber.getText();
+		String inputCritter = critterChoiceBox.getValue();
+		System.out.println(inputNum + " " + inputCritter);
+		try {
+			int num = Integer.parseInt(inputNum);
+			for(int i = 0; i < num; ++i) {
+				Critter.makeCritter(inputCritter);
+			}
+		}catch(NumberFormatException | InvalidCritterException e){
+			//For now leave as empty to ignore any strange changes
 		}
 	}
 }
--- src/assignment4/Main.java
+++ src/assignment4/Main.java
@@ -1,41 +1,95 @@
 package assignment4;
 
+import java.io.File;
+import java.lang.reflect.Modifier;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
+public class Main extends Application {
 
-public class Main extends Application{
+	private static String myPackage; // package of Critter file. Critter cannot be in default pkg.
+	public static ObservableList<String> critter_list;
+
+	private static String pkgLoc;
+
+	// Gets the package name. The usage assumes that Critter and its subclasses are
+	// all in the same package.
+	static {
+		myPackage = Critter.class.getPackage().toString().split(" ")[1];
+		pkgLoc = Paths.get("").toAbsolutePath().toString().replace("\\", "\\\\") + "\\\\" + "bin\\\\" + myPackage;
+		critter_list = makeCritterList();
+	}
+
+	public static ObservableList<String> makeCritterList() {
+		File f = new File(pkgLoc);
+		String[] subFiles = f.list();
+		List<String> findClasses = new ArrayList<String>();
+		List<String> findCritters = new ArrayList<String>();
+		for (String s : subFiles) {
+			if (s.endsWith(".class")) {
+				findClasses.add(s);
+			}
+		}
+		for (String s : findClasses) {
+			String critName = s.substring(0, s.indexOf(".class"));
+			try {
+				Class<?> check_class = Class.forName(myPackage + '.' + critName);
+				int mod = check_class.getModifiers();
+				if (Modifier.isAbstract(mod) || Modifier.isPrivate(mod)) {
+					continue;
+				}
+				Class<?> critter_class = Critter.class;
+				if (critter_class.isAssignableFrom(check_class)) {
+					findCritters.add(critName);
+					// System.out.println(critName);
+				}
+			} catch (ClassNotFoundException e) {
+				// Shouldn't happen
+				continue;
+			}
+		}
+		return FXCollections.observableArrayList(findCritters);
+	}
 
 	@Override
 	public void start(Stage primaryStage) {
-		
+
 		try {
-			
-			for(int i = 0; i < 10; i++) {
-//				Critter.makeCritter("Craig");
-//				Critter.makeCritter("Critter1");
-//				Critter.makeCritter("Critter2");
+
+			for (int i = 0; i < 10; i++) {
+				// Critter.makeCritter("Craig");
+				// Critter.makeCritter("Critter1");
+				// Critter.makeCritter("Critter2");
 				Critter.makeCritter("Critter3");
-//				Critter.makeCritter("Critter4");
+				// Critter.makeCritter("Critter4");
 			}
 
-			
 			Parent root = FXMLLoader.load(getClass().getResource("AppScene.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			
+
 			primaryStage.setTitle("Critters Project");
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
+		for (String s : critter_list) {
+			System.out.println(s);
+		}
 		launch(args);
 	}
 }
